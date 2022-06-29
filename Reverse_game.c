cd65e44a72b530725de3e2169d2b9ba31e933a4c
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-06-29 22:08:58 +0530

    #include<stdio.h> int pal(int n) {     int rev=0,r;     while(n!=0)     {         r=n%10;         rev=rev*10+r;         n=n/10;     }     return rev; } int main() {     int n,c=0;     scanf("%d",&n);
        int a[n],i;
        for(i=0;i<n;i++)
        { 
            scanf("%d",&a[i]);
        }
        for(i=0;i<n;i++)
        {
            
        printf("%d ",pal(a[i]));
    }
    }

---
+++ Reverse_game.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+int pal(int n)
+{
+    int rev=0,r;
+    while(n!=0)
+    {
+        r=n%10;
+        rev=rev*10+r;
+        n=n/10;
+    }
+    return rev;
+}
+int main()
+{
+    int n,c=0;
+    scanf("%d",&n);
+    
+    int a[n],i;
+    for(i=0;i<n;i++)
+    { 
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        
+    printf("%d ",pal(a[i]));
+}
+}
