b149a94703d3b667353a74a9d0850fb3d6f8e0d8
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-07-01 14:16:30 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {     for(j=1;j<=n-2;j++)     {             printf("%d",j);      }         for(j=n-3;j>=1;j--)      {            printf("%d",j);      }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -5,15 +5,15 @@ int main()
     scanf("%d",&n);
     for(i=1;i<=n;i++)
     {
-        for(j=1;j<=n-2;j++)
-        {
-        printf("%d",j);
-    }
-    for(j=1;j<n-2;j++)
+    for(j=1;j<=n-2;j++)
     {
-        printf("%d",j);
-    }
-    printf("
+            printf("%d",j);
+     }
+        for(j=n-3;j>=1;j--)
+     {
+           printf("%d",j); 
+     }
+        printf("
 ");
-}
+    }
 }
