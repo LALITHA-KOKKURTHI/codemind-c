53a6a2dc000b07aa8a65f81ea7e25e3cd18f88ed
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-07-16 14:04:47 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],o=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==1)         {
                o=o+arr[i];
            }
        }
        
        printf("%d",o);
    }

---
+++ ODD_Sum.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],o=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==1)
+        {
+            
+            o=o+arr[i];
+        }
+    }
+    
+    printf("%d",o);
+}
