c09acf38d332bbc8a3a035579289d30ab9de1f46
LALITHA-KOKKURTHI <96776984+LALITHA-KOKKURTHI@users.noreply.github.com>
2022-07-16 13:55:36 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],e=0,o=0,diff=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2==0)         {             e=e+arr[i];         }         else         {             o=o+arr[i];         }     }     if(e>o)     {         diff=e-o;     }     else     {         diff=o-e;     }     printf("%d",diff); }

---
+++ Absolute_difference_b/w_sum_of_even_and_sum_of_odd_elements.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],e=0,o=0,diff=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2==0)
+        {
+            e=e+arr[i];
+        }
+        else
+        {
+            o=o+arr[i];
+        }
+    }
+    if(e>o)
+    {
+        diff=e-o;
+    }
+    else
+    {
+        diff=o-e;
+    }
+    printf("%d",diff);
+}
